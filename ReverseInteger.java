6a50cdf7984d383aaedd198be46f96d98a1fe24e
yiyiyayahu <yuanjie_qu@hotmail.com>
2014-11-12 22:27:22 -0800

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -3,6 +3,16 @@ Reverse digits of an integer.
 
 Example1: x = 123, return 321
 Example2: x = -123, return -321
+
+Spoilers:
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 public class Solution {
     public int reverse(int x) {
@@ -15,6 +25,7 @@ public class Solution {
         int result = 0, digit = 0;
         while(x != 0) {
             digit = x % 10;
+            //To see if this is an overflow or not
             if(result > (Integer.MAX_VALUE - digit) / 10) {
                 result = 0;
                 x = 0;
