24cbe6193d6943752ac413accc64e48c20181786
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-01-02 20:58:24 -0800

    Update ExcelSheetColumnNumber.java

--- ExcelSheetColumnNumber.java
+++ ExcelSheetColumnNumber.java
@@ -20,39 +20,8 @@ public class Solution {
         if(s == null) return result;
         
         for(int i = 0; i < s.length(); i++) {
-            result = result * 26 + toNum(s.charAt(i));
+            result = result * 26 + s.charAt(i) - 'A' + 1;
         }
         return result;
-    }
-    public int toNum(char c) {
-        switch(c) {
-            case 'A' : return 1;
-            case 'B' : return 2;
-            case 'C' : return 3;
-            case 'D' : return 4;
-            case 'E' : return 5;
-            case 'F' : return 6;
-            case 'G' : return 7;
-            case 'H' : return 8;
-            case 'I' : return 9;
-            case 'J' : return 10;
-            case 'K' : return 11;
-            case 'L' : return 12;
-            case 'M' : return 13;
-            case 'N' : return 14;
-            case 'O' : return 15;
-            case 'P' : return 16;
-            case 'Q' : return 17;
-            case 'R' : return 18;
-            case 'S' : return 19;
-            case 'T' : return 20;
-            case 'U' : return 21;
-            case 'V' : return 22;
-            case 'W' : return 23;
-            case 'X' : return 24;
-            case 'Y' : return 25;
-            case 'Z' : return 26;
-        }
-        return 0;
     }
 }
