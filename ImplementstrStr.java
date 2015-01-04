24e70ade7ece0109a892b759c3243888eef7e16c
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-01-04 00:11:11 -0800

    Update ImplementstrStr.java

--- ImplementstrStr.java
+++ ImplementstrStr.java
@@ -30,3 +30,15 @@ public class Solution {
         return -1;
     }
 }
+
+
+/*
+Solution:
+O(nm) runtime, O(1) space – Brute force:
+
+You could demonstrate to your interviewer that this problem can be solved using known efficient algorithms such as Rabin-Karp algorithm, KMP algorithm, and the Boyer- Moore algorithm. Since these algorithms are usually studied in an advanced algorithms class, it is sufficient to solve it using the most direct method in an interview – The brute force method.
+
+The brute force method is straightforward to implement. We scan the needle with the haystack from its first position and start matching all subsequent letters one by one. If one of the letters does not match, we start over again with the next position in the haystack.
+
+The key is to implement the solution cleanly without dealing with each edge case separately.
+*/
