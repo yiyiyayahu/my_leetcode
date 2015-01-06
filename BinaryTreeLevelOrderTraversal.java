8c8abfb8ace20b5f84c1a93a3d1418d4507621ca
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-01-06 14:38:26 -0800

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -58,3 +58,30 @@ public class Solution {
         }
     }
 }
+
+
+/*
+Binary Tree Level Order Traversal II
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its bottom-up level order traversal as:
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+
+Solution:
+change line52 result.add(level_list);
+to result.add(0, level_list);
