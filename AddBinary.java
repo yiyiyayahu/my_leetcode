c06a197b3739c0cd05e77d93cf63e198343d466b
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-01-07 23:00:04 -0800

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -1,53 +1,43 @@
+/*
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
+
 public class Solution {
     public String addBinary(String a, String b) {
         if(a == null) return b; 
         if(b == null) return a;
         
-        int la = a.length() - 1;
-        int lb = b.length() - 1;
-        
+        int la = a.length() - 1, lb = b.length() - 1;
         int carry = 0, result = 0, na = 0, nb = 0;
         StringBuilder sb = new StringBuilder();
         
-        while(la >= 0 && lb >= 0) {
-            na = a.charAt(la) - '0';
-            nb = b.charAt(lb) - '0';
+        int maxLen = la > lb ? la : lb;
+        
+        while(maxLen >= 0) {
+            if(la >= 0) na = a.charAt(la) - '0';
+            else na = 0;
+            if(lb >= 0) nb = b.charAt(lb) - '0';
+            else nb = 0;
             
             result = na ^ nb ^ carry;
             if(carry == 0) carry = na & nb;
             else carry &= na | nb;
-            sb.append(result + "");
             
+            sb.append(result == 0? '0':'1');
+            
+            maxLen --;
             la --;
             lb --;
         }
-        
-        if(la < 0 && lb >= 0) {
-            while(lb >= 0) {
-                if(carry == 0) sb.append(b.charAt(lb));
-                else {
-                    nb = b.charAt(lb) - '0';
-                    result = nb ^ carry;
-                    carry &= nb;
-                    sb.append(result + "");
-                }
-                lb --;
-            }
-        }
-        
-        if(lb < 0 && la >= 0) {
-            while(la >= 0) {
-                if(carry == 0) sb.append(a.charAt(la));
-                else {
-                    na = a.charAt(la) - '0';
-                    result = na ^ carry;
-                    carry &= na;
-                    sb.append(result + "");
-                }
-                la --;
-            }
-        }
+
         if(la < 0 && lb < 0 && carry ==1) sb.append(carry+"");
         return sb.reverse().toString();
     }
 }
+
+/*Remember don't use (result + ""), since it will create a new string object each time*/
