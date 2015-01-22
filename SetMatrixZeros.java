f01c3b0e1c33ce469893e18cc77a373f33bd8bef
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-01-21 23:40:49 -0800

    Update SetMatrixZeros.java

--- SetMatrixZeros.java
+++ SetMatrixZeros.java
@@ -33,3 +33,46 @@ public class Solution {
         }
     }
 }
+
+/*constant space*/
+/*need to add two booleans to check if the first row has zeros!!!*/
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) return;
+        boolean first_row_has_zero = false;
+        boolean first_col_has_zero = false;
+        for(int j = 0; j < matrix[0].length; j++) {
+            if(matrix[0][j] == 0) first_row_has_zero = true;
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            if(matrix[i][0] == 0) first_col_has_zero = true;
+        }
+
+        if(matrix.length > 1 && matrix[0].length > 1) {
+            for(int i = 1; i < matrix.length; i++) {
+                for(int j = 1; j < matrix[0].length; j++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+            
+            for(int i = 1; i < matrix.length; i++) {
+                for(int j = 1; j < matrix[0].length; j++) {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(first_row_has_zero) {
+            for(int j = 0; j < matrix[0].length; j++) 
+                matrix[0][j] = 0;
+        }
+        if(first_col_has_zero) {
+            for(int i = 0; i < matrix.length; i++) 
+                matrix[i][0] = 0;
+        }
+    }
+}
