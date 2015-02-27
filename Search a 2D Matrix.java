fa0c9392046b3230e64acd063b66effaf4019c1d
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-02-26 21:48:26 -0800

    Update Search a 2D Matrix.java

--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -15,6 +15,27 @@ Consider the following matrix:
 Given target = 3, return true.
 */
 
+//二分法，但是把这个二维数组转成一维的了，不晓得行不行，时间复杂度是O(log(nm))
+public class Solution {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if(matrix == null || matrix.length == 0) return false;
+		
+        int rows = matrix.length, cols = matrix[0].length;
+		return searchMatrixHelper(matrix, target, 0, rows * cols - 1);
+	}
+	
+	public boolean searchMatrixHelper(int[][] matrix, int target, int start, int end) {
+		if(start > end) return false;
+		int mid = (start + end)/2;
+		int i = mid / matrix[0].length;
+		int j = mid % matrix[0].length;
+		if(target > matrix[i][j]) return searchMatrixHelper(matrix, target, mid + 1, end);
+		else if(target < matrix[i][j]) return searchMatrixHelper(matrix, target, start, mid - 1);
+		else return true;
+	}
+}
+
+//previous 时间复杂度是O(nm)
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0) return false;
@@ -39,4 +60,4 @@ public class Solution {
         return false;
     }
 }
-//应该用二分法的，明天想想
+
