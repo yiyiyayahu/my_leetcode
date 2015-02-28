09d1bdd0f918b680304d0790998b9de7c8d22a59
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-02-28 14:01:50 -0800

    Update RemoveDuplicatesfromSortedArrayII.java

--- RemoveDuplicatesfromSortedArrayII.java
+++ RemoveDuplicatesfromSortedArrayII.java
@@ -8,6 +8,9 @@ Given sorted array A = [1,1,1,2,2,3],
 Your function should return length = 5, and A is now [1,1,2,2,3].
 */
 
+/*
+用一个index来作为新的array的index~ 和I差不多，只是这里两次的OK，就多加了一个times变量，细节要注意 
+*/
 public class Solution {
     public int removeDuplicates(int[] A) {
         if(A == null || A.length == 0) return 0;
