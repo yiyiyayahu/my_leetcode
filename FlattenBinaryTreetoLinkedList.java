784a8f62ec8761aeadd3b89c5f50decd0fa069cd
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-14 20:48:43 -0700

    Create FlattenBinaryTreetoLinkedList.java

---
+++ FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+/*
+这相当于遍历了两遍，应该可以inplace改的，再想想
+*/
+public class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+    	List<Integer> list = preOrderTraversal(root);
+    	TreeNode node = root; 
+
+    	for(int i= 1; i < list.size(); i++) {
+    		node.right = new TreeNode(list.get(i));
+    		node = node.right;
+    	}
+    	root.left = null;
+    }
+	
+	public List<Integer> preOrderTraversal(TreeNode root) {
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		List<Integer> result = new ArrayList<Integer>();
+		if(root == null) return result;
+		
+		s.push(root);
+
+		while(!s.isEmpty()) {
+			TreeNode tmp = s.pop();
+			result.add(tmp.val);
+			if(tmp.right != null) s.push(tmp.right);
+			if(tmp.left != null) s.push(tmp.left);
+		}
+		return result;
+ 	}
+}
+
+
+/*
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+    	List<Integer> list = preOrderTraversal(root);
+    	TreeNode node = root; 
+    	node = node.right;
+
+    	for(int i= 1; i < list.size(); i++) {
+    		node = new TreeNode(list.get(i));
+    		node = node.right;
+    	}
+    	root.left = null;
+    }
+    最开始是这样写的，怎么都不对
+*/
