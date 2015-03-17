49297ff8d860417e6573391b9c92ce4efcd29702
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-16 22:00:33 -0700

    Update Pascal'sTriangleII.java

--- Pascal'sTriangleII.java
+++ Pascal'sTriangleII.java
@@ -15,6 +15,27 @@ Could you optimize your algorithm to use only O(k) extra space?
 
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
+	if(rowIndex < 0) return null;
+	List<Integer> list = new ArrayList<Integer>(rowIndex + 1);
+	
+	for(int i = 0; i < rowIndex+1; i++) {
+		list.add(i, 0);
+	}
+	list.set(0, 1);
+	
+	for(int i = 1; i <= rowIndex; i++) {
+		list.set(i,1);
+		for(int j = i-1; j > 0; j--) {
+			int tmp = list.get(j) + list.get(j-1);	
+			list.set(j, tmp);
+		}
+	}
+	return list; 
+    }
+}
+
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new ArrayList<Integer>();
     	if(rowIndex < 0) return list;
     	list.add(1);
