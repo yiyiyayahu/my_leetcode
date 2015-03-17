8da90683f13f827bfa0455749291a78ce0685203
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-16 22:05:27 -0700

    Update Pascal'sTriangleII.java

--- Pascal'sTriangleII.java
+++ Pascal'sTriangleII.java
@@ -9,10 +9,8 @@ Could you optimize your algorithm to use only O(k) extra space?
 */
 
 /*
-和I差不多。。。但是我不确定我的空间复杂度OK么。。。。
-应该可以这个array in-place的滚动向前得到，再想想
+赶脚好难想。最开始想错了，后来这个也是参考人家的才做粗来的。
 */
-
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
 	if(rowIndex < 0) return null;
@@ -34,6 +32,38 @@ public class Solution {
     }
 }
 
+/*错误版本*/
+/*这个版本的问题是从后面往前面算，但是往往我的第n位已经更新过了，这样再算第n-1位的结果就是错的。。。不晓得怎么改
+原来想法是如果rowIndex=3：
+|               1 |
+|            1，1 |
+|         1，2，1 |
+|      1，3，3，1 |
+*/
+	public static List<Integer> getRow(int rowIndex) {
+		if(rowIndex < 0) return null;
+		
+		List<Integer> list = new ArrayList<Integer>(rowIndex + 1);
+		
+		for(int i = 0; i < rowIndex+1; i++) {
+			list.add(i, 0);
+		}
+		list.set(rowIndex, 1);
+		
+		for(int i = 0; i < rowIndex; i++) {
+			int tmp = 0;
+			for(int j = rowIndex - 1; j >= rowIndex-1-i; j--) {
+				tmp = list.get(j) + list.get(j+1);				
+				list.set(j, tmp);
+			}			
+		}
+		return list;
+	}
+	
+/*
+和I差不多。。。但是我的空间复杂度不好啊，比如rowIndex=k，那用掉的空间其实是1+2+3+...+k应该是O(k^2)
+应该可以这个array in-place的滚动向前得到，再想想
+*/
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new ArrayList<Integer>();
