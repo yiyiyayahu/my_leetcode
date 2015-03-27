6dea0d29b2c57c6c04b32250e40f20ddd431b6a8
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-26 19:26:42 -0700

    Update BinaryTreePostorderTraversal.java

--- BinaryTreePostorderTraversal.java
+++ BinaryTreePostorderTraversal.java
@@ -29,6 +29,7 @@ Note: Recursive solution is trivial, could you do it iteratively?
 我现在是用一个ArrayList来存之前左右子树都遍历过的节点，如果遇到了，就直接输出。但是不晓得这样做好不好，实在没想出其他好方法
 时间复杂度，诶，貌似都是n^2级别的，因为我每次要判断下这个节点有没有之前遍历过。。。这，咋办
 网上有个解法是遍历过就把这个节点的left和right都设为null，这样就把树改了呀，不太好吧？
+喔喔，leetcode给出的答案是双指针，然后track到底是在向下遍历，还是已经左边遍历过了，还是已经右边遍历过了。。。
 空间复杂度O(n)
 */
 public class Solution {
