9a90f9def963735aaaee0b60c49d7c81d84dd007
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-28 15:27:03 -0700

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -12,6 +12,24 @@ Could you do this in-place?
 但是我开始还是错了，没有考虑到如果n是奇数，那其实在最中间那一列，也就是j=n/2这一列是不变的，因为我没有挪动过
 改过之后就多加了一步判断，如果是奇数，那j是可以等于n/2的
 */
+//refactor了一下，不用这么多变量存，开始只是没有特别考虑清楚
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int maxJ = (n%2 == 0) ? n/2 : n/2+1;
+        for(int i = 0; i < n/2; i ++) {
+            for(int j = 0; j < maxJ; j ++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+}
+
+//old:
 public class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
