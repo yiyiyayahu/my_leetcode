5ccf3e1310d80dcdd0c8cf6c311cb03098d676d6
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-30 22:42:50 -0700

    Update ReverseWordsinaStringII.java

--- ReverseWordsinaStringII.java
+++ ReverseWordsinaStringII.java
@@ -13,7 +13,44 @@ Related problem: Rotate Array
 */
 
 /*
-code写的特别乱，而且没有想好怎么才能in place的换
+in place呢，一种方法就是先把这个array整个reverse，然后再对每个单词reverse回来
+"the sky is blue" -> "eulb si yks eht" -> "blue is sky the"
+*/
+public class Solution {
+    public void reverseWords(char[] s) {
+        if(s == null || s.length == 0) return;
+        int len = s.length;
+        
+        int i = 0, j = len - 1;
+        while(i <= j) {
+            swap(s, i++, j--);
+        }
+        
+        int start = 0;
+        for(int k = 0; k < len; k++) {
+            if(s[k] == ' ') {
+                int tmp = k - 1;              //这里注意不能直接对k进行操作，不然就和外面的循环冲突，进入死循环
+                while(start <= tmp) {
+                    swap(s, start ++, tmp --);
+                }
+                start = k + 1;
+            }
+        }
+        int end = len - 1;
+        while(start <= end) {
+            swap(s, start ++, end--);
+        }
+        
+    }
+    
+    public void swap(char[] s, int i, int j) {
+        char tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+}
+/*
+code写的有点乱，而且没有想好怎么才能in place的换
 */
 
 public class Solution {
