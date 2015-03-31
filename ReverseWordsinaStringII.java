adb89823303f3387e6e8ee54517fd1ee944bad22
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-30 21:30:53 -0700

    Create ReverseWordsinaStringII.java

---
+++ ReverseWordsinaStringII.java
@@ -0,0 +1,49 @@
+/*
+Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+
+The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Could you do it in-place without allocating extra space?
+
+Related problem: Rotate Array
+*/
+
+/*
+code写的特别乱，而且没有想好怎么才能in place的换
+*/
+
+public class Solution {
+    public void reverseWords(char[] s) {
+        if(s == null || s.length == 0) return;
+        boolean isOneWord = true;
+        int len = s.length;
+        char[] tmp = new char[len];
+        
+        int j = len - 1;
+        int start = 0;
+        for(int i = 0; i < len-1; i++) {
+            if(s[i] == ' ') {
+            	isOneWord = false;
+            	tmp[j-(i-start)] = ' ';
+                for(int k = start; k < i; k++) {
+                    tmp[j - (i-k-1)] = s[k];
+                }
+                j = j - (i-start) - 1;
+                start = i + 1;
+            }
+        }
+        
+        while(start < len) {
+        	tmp[j-(len-1-start)] = s[start++];
+        }
+        if(!isOneWord) {	        
+	        for(int i = 0; i < len; i++) {
+	            s[i] = tmp[i];
+	        }
+        }
+    }
+}
