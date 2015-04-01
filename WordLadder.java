91f9351bb3ea879a408c6189acb84da905bbc9e2
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-03-31 21:52:57 -0700

    Update WordLadder.java

--- WordLadder.java
+++ WordLadder.java
@@ -19,26 +19,30 @@ All words contain only lowercase alphabetic characters.
 */
 
 /*
-最开始的写法是这样的，但是会Time Limit Exceeded，我觉得是因为我每一次都要遍历整个dict，用时太长
-比如如果第一层s1出现过，其实就可以从dict中删掉了，防止第二层再出现s2，反正也不是最优解。
+最开始的写法是这样的，但是会Time Limit Exceeded
 */
 public class Solution {
     public int ladderLength(String start, String end, Set<String> dict) {
         if(start == null) return 0;
         if(start.length() != end.length()) return 0;
-
+        
+        Queue<String> queue = new LinkedList<String>();
+      
         int result = 0;
         if(isOneLetterDiff(start, end)) return 2;
         if(dict.isEmpty()) return 0;
+        Set<String> tmpDict = new HashSet<String>(dict);
         for(String s : dict) {
-            if(isOneLetterDiff(start, s)) {
-            	Set<String> tmpDict = new HashSet<String>(dict);
+            if(isOneLetterDiff(start, s)) {          	
                 tmpDict.remove(s);
-                if(result == 0) result = ladderLength(s, end, tmpDict) + 1;
-                else result = Math.min( ladderLength(s, end, tmpDict) + 1, result);
+                queue.add(s);
             }
         }
 
+        for(String s : queue) {
+            if(result == 0) result = ladderLength(s, end, tmpDict) + 1;
+            else result = Math.min( ladderLength(s, end, tmpDict) + 1, result);
+        }
         return result;
     }
     
