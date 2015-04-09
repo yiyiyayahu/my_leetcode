4f332b47865a3379cc86de20f6228be74597abc9
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-04-08 20:18:53 -0700

    Update LongestSubstringwithAtMostTwoDistinctCharacters.java

--- LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -7,6 +7,9 @@ T is "ece" which its length is 3.
 */
 /*
 确定这是一道Hard难度的题吗？还是我打开方式不对？时间复杂度不好？
+我能想到的优化就是if(i > 0 && s.charAt(i) == s.charAt(i-1)) continue;
+也就是这种情况abbbbbbcccc，a是要往后找的，b呢，也是要往后找的，但是第二个b就不用再bother的找了
+其他的我也想不出了啊。复杂度应该是O(n)吧
 */
 public class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
@@ -16,6 +19,8 @@ public class Solution {
         
         int result = 0;
         for(int i = 0; i < len; i++) {
+            if(i > 0 && s.charAt(i) == s.charAt(i-1)) continue;
+            
             char curr = s.charAt(i);
             int j = i + 1, tmp = 1, numOfLetters = 1;
             char next = curr;
