26b895189469f35607102a12e8bbd5b374cc57d4
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-04-14 23:03:41 -0700

    Update OneEditDistance.java

--- OneEditDistance.java
+++ OneEditDistance.java
@@ -3,6 +3,65 @@ Given two strings S and T, determine if they are both one edit distance apart.
 */
 
 /*
+这样code就简洁一些了，但是网上的比我写的还简洁
+*/
+
+public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        if(s == null || t == null) return false;
+
+        if(s.length() > t.length()) return helper(t, s);
+        else return helper(s, t);
+    }
+    
+    public boolean helper(String s, String t) {
+        int len1 = s.length(), len2 = t.length();
+        
+        if(len2 - len1 > 1) return false;
+        
+        boolean isOneDiff = false;
+        for(int i = 0, j = 0; i < len1; i++, j++) {
+            if(s.charAt(i) != t.charAt(j)) {
+                if(isOneDiff) return false;
+                isOneDiff = true;
+                if(len1 < len2) i --;
+            }
+        }
+        return isOneDiff || (len1 < len2);
+    }
+}
+
+
+public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        if(s == null || t == null) return false;
+
+        if(s.length() > t.length()) return helper(t, s);
+        else return helper(s, t);
+    }
+    
+    public boolean helper(String s, String t) {
+        int len1 = s.length(), len2 = t.length();
+        
+        if(len2 - len1 > 1) return false;
+        
+        boolean isOneDiff = false;
+        for(int i = 0, j = 0; i < len1; i++, j++) {
+            if(s.charAt(i) != t.charAt(j)) {
+                if(isOneDiff) return false;
+                isOneDiff = true;
+                if(len1 < len2) {
+                	j ++;           	
+                	if(j < len2 && s.charAt(i) != t.charAt(j)) return false;
+                }
+            }
+        }
+        if(!isOneDiff && len1 < len2) return true;
+        if(isOneDiff) return true;
+        else return false;
+    }
+}
+/*
 code写的特别乱啊。要refactor一下
 s.substring(0,i-1).equals(t.substring(0,i-1)这里又忘记先检查是不是i>1了
 */
