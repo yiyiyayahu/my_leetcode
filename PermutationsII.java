69d0734ebf06a7474fa60899960081d2e01c0c8b
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-04-24 18:54:48 -0700

    Update PermutationsII.java

--- PermutationsII.java
+++ PermutationsII.java
@@ -1,4 +1,49 @@
 /*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[1,1,2], [1,2,1], and [2,1,1].
+*/
+
+/*
+之前iterative的做法不是特别清楚应该怎么改 T.T
+
+recursive我觉得我理解的还不够，应该再多多练习，就是这种NP的题
+*/
+
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] num) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	if(num == null || num.length == 0) return result;
+    	List<Integer> list = new ArrayList<Integer>();
+    	boolean[] visited = new boolean[num.length];
+    	
+    	Arrays.sort(num);
+    	helper(num, list, result, visited);
+		return result;
+    }
+    
+    public static void helper(int[] num, List<Integer> list, List<List<Integer>> result, boolean[] visited) {
+    	if(list.size() == num.length) {
+    		result.add(new ArrayList<Integer>(list));
+    		return;
+    	}
+    	
+    	for(int i = 0; i < num.length; i++) {
+    		if(!visited[i]) {
+    		    if(i > 0 && num[i] == num[i-1] && visited[i-1]) continue;
+    			visited[i] = true;
+	    		list.add(num[i]);
+	    		helper(num, list, result, visited);
+	    		list.remove(list.size() - 1);
+	    		visited[i] = false;
+    		}
+    	}
+    }
+}
+
+/*
 Only added some lines of code from I:
 1) sort the array first
 2) if the digit is seen before: 加到重复元素的后面
