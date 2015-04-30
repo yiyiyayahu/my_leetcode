ba15e99949a55f01f9c32abe0c5888e24b89b72a
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-04-29 23:17:25 -0700

    Update InsertionSortList.java

--- InsertionSortList.java
+++ InsertionSortList.java
@@ -1,4 +1,45 @@
   /*
+  Sort a linked list using insertion sort.
+  */
+  
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode n = head;
+        
+        while(n != null) {
+            int val = n.val;
+            ListNode tmp = head;
+            while(tmp.next != n && tmp.val < val) {
+                tmp = tmp.next;
+            }
+            if(tmp.val >= val) {
+                int preVal = tmp.val;
+                tmp.val = val;
+                while(tmp != n) {
+                	tmp = tmp.next;
+                	int currVal = tmp.val;
+                	tmp.val = preVal;
+                	preVal = currVal;
+                }
+            }
+            n = n.next;
+        }
+        
+        return head;
+    }
+}
+  
+  /*
   先写了一个对array的insertionSort
   开始理解不是特别对，不是swap，而是插入
   比如5,1,4,2,3->1,5,4,2,3->1,4,5,2,3->1,2,4,5,3->1,2,3,4,5
