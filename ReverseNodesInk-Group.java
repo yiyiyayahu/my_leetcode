2e8f215b5c1ef9269d96f285de39fa1b8b4ba4c0
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-06 22:31:06 -0700

    Update ReverseNodesInk-Group.java

--- ReverseNodesInk-Group.java
+++ ReverseNodesInk-Group.java
@@ -16,6 +16,62 @@ For k = 3, you should return: 3->2->1->4->5
 */
 
 /*
+只refactor成下面这样，还是略差
+
+我的做法是比如 1->2->3->4->5
+
+start=1, end=3, 然后1到3之前reverse : 2->1->3->4->5
+然后再start=3, end=5, 3到5之前reverse: 4->3->5, 然后再把tmpTail挪到1的位置，指向4
+
+我觉得这个做法不是很好，但是没想到怎么改进
+
+我之前没有考虑一个问题，就是如果k大于这个linkedlist的长度怎么办，后来加了一句这个：
+if(count < k && start == head) return head;
+*/
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+    	ListNode start = head, end = head;
+    	int count = 0;
+    	ListNode ret = null, tail = null, tmpTail = null;
+    	while(end != null) {
+    		end = end.next;
+    		count ++;
+
+            if(count == k) {
+        		if(start == head) {
+        			ret = reverse(start, end);   	
+        			tmpTail = ret;
+        		} else {
+        			tail = reverse(start, end);
+        			for(int i = 0; i < k-1; i++) {
+        				tmpTail = tmpTail.next;
+        			}
+        			tmpTail.next = tail;
+        			tmpTail = tail;
+        		}
+        		
+        		start = end;
+        		count = 0;
+            }
+    	}
+    	if(count < k && start == head) return head;
+    	return ret;
+    }
+
+    public ListNode reverse(ListNode start, ListNode end) {
+        ListNode n = start;
+        ListNode newtail = end;
+        
+        while(n != end) {
+            ListNode next = n.next;
+            n.next = newtail;
+            newtail = n;
+            n = next;
+        }
+        return newtail;
+    }
+}
+/*
 唉，代码写的又臭又长，赶紧refactor
 */
 /**
