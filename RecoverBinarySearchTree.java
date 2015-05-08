5e5acf70940315b9716480a980ab4fd7cc891727
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-07 23:24:08 -0700

    Update RecoverBinarySearchTree.java

--- RecoverBinarySearchTree.java
+++ RecoverBinarySearchTree.java
@@ -8,6 +8,46 @@ A solution using O(n) space is pretty straight forward. Could you devise a const
 */
 
 /*
+好难啊，其实我也想到用两个node，first和second来保存哪里出错了，然后这里的traversal应该是inorder的
+找到这两个node之后只要值换一下就行了
+但是，但是，怎么找着两个node不晓得诶
+http://yucoding.blogspot.com/2013/03/leetcode-question-75-recover-binary.html
+http://fisherlei.blogspot.com/2012/12/leetcode-recover-binary-search-tree.html
+
+其实用双指针遍历，prev和curr，prev是curr的inorder之前的node。因为BST的inorder出来的结果应该是一直increasing的，
+那么如果prev.val > curr.val, 就是有问题的。
+yucoding的blog用的是recursive的做法。我自己没写出，再好好体会下。
+*/
+public class Solution {
+    TreeNode first;
+    TreeNode second;
+    TreeNode prev;
+    public void recoverTree(TreeNode root) {
+        if(root == null) return;
+        first = null; second = null; prev = null;
+        inorder(root);
+        if(first != null && second != null) {
+            int tmp = first.val;
+            first.val = second.val;
+            second.val = tmp;
+        }
+    }
+    public void inorder(TreeNode curr) {
+        if(curr == null) return;
+        //prev is the inorder previous node from curr
+        inorder(curr.left);
+        if(prev == null) prev = curr;
+        else {
+            if(prev.val > curr.val) {
+                if(first == null) first = prev;
+                second = curr;
+            }
+            prev = curr;
+        }
+        inorder(curr.right);
+    }
+}
+/*
 先写的O(n)的解法，为了保证记录之前node的顺序，所以也弄了一个ListNode<TreeNode>
 用InorderTraversal遍历。就直接写的recursive的简单解法
 O(1)的想不粗。。。
