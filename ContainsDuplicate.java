5eb4ba3b1e9a99eab2303d247ac8ced1c38a58d7
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-27 16:03:30 -0700

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -3,7 +3,20 @@ Given an array of integers, find if the array contains any duplicates.
 Your function should return true if any value appears at least twice in the array, 
 and it should return false if every element is distinct.
 */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0) return false;
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+}
 
+/*
+这里其实理解错了，只有every element is distinct的时候才return false，我以为要所有的至少出现两次呢
+*/
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         if(nums == null || nums.length == 0) return false;
