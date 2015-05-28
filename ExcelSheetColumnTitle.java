c74c890c9d882be10bb21efa63ed679a6f8c27e8
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-27 23:50:27 -0700

    Update ExcelSheetColumnTitle.java

--- ExcelSheetColumnTitle.java
+++ ExcelSheetColumnTitle.java
@@ -1,13 +1,37 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+*/
+
+/*
+A的ascii码是65
+*/
+public class Solution {
     public String convertToTitle(int n) {
         StringBuffer sb = new StringBuffer();
+        int tmp = 0;
         while(n != 0) {
-                n = n%26;
-                sb.append(0, getChar(n));
-                n = n / 26;
+            tmp = n%26;
+            if(tmp != 0) sb.insert(0, getChar(tmp));
+            else {
+            	tmp = 26;
+            	sb.insert(0, 'Z');
+            }
+            n = (n-tmp) / 26;
         }
         return sb.toString();
     }
-    public char getChar(int n) {
+    public static char getChar(int n) {
         char c = (char) (n + 64);
         return c;
     }
+}
