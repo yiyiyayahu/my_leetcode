ec14f2d64a7b54be8c8440cb4d4b65e59056155e
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-27 23:53:39 -0700

    Update ExcelSheetColumnTitle.java

--- ExcelSheetColumnTitle.java
+++ ExcelSheetColumnTitle.java
@@ -14,7 +14,26 @@ For example:
 
 /*
 A的ascii码是65
+注意n=26的情况！
 */
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuffer sb = new StringBuffer();
+        int tmp = 0;
+        while(n != 0) {
+            tmp = (n%26==0) ? 26 : n%26;
+            sb.insert(0, getChar(tmp));
+            n = (n-tmp) / 26;
+        }
+        return sb.toString();
+    }
+    public char getChar(int n) {
+        char c = (char) (n + 64);
+        return c;
+    }
+}
+
+
 public class Solution {
     public String convertToTitle(int n) {
         StringBuffer sb = new StringBuffer();
