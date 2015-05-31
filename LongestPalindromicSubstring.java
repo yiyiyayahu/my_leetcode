fa1a4e3c25b3f4510858120653934ac41eea6cdd
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-05-30 20:25:19 -0700

    Create LongestPalindromicSubstring.java

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,39 @@
+/*
+Given a string S, find the longest palindromic substring in S. 
+You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+/*
+我想的是dp，但是会出现TLE
+思路是：
+1) d[i][i] = true
+2) d[i][i+1] = true if(s[i] == s[i+1])
+3) d[i][j] = true if(s[i]==s[j] && d[i+1][j-1]==true)
+*/
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0) return s;
+        int len = s.length();
+        boolean[][] d = new boolean[len][len];
+        String result = "";
+        int maxLen = 0;
+        
+        for(int i = len-1; i >= 0; i--) {
+            for(int j = i; j < len; j++) {
+                if(i == j) d[i][j] = true;
+                else if(j == i+1 && s.charAt(i) == s.charAt(j)) d[i][j] = true;
+                else {
+                    if(i + 1 >= len || j - 1 <= 0) continue;
+                    if(s.charAt(i) == s.charAt(j) && d[i+1][j-1]== true) {
+                        d[i][j] = true;
+                    } 
+                }
+                if(d[i][j] == true && j - i >= maxLen) {
+                	result = s.substring(i, j + 1);
+                	maxLen = j - i;
+                }
+             }
+        }
+        return result;
+    }
+}
