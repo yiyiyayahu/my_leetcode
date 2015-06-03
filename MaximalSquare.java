3140602a83b7f1e0add99f6101493c48af1ef11d
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-03 15:25:47 -0700

    Update MaximalSquare.java

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -10,6 +10,15 @@ For example, given the following matrix:
 
 Return 4. 
 */
+
+/*
+dp的做法：
+dp[0][j]= matrix[0][j]-'0'
+dp[i][0] = matrix[i][0]-'0'
+dp[i][j] = 0 if matrix[i][j]=='0'
+         = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+但是可以优化成一维数组来维护。再想想
+*/
 public class Solution {
     public int maximalSquare(char[][] matrix) {
         if(matrix == null) return 0;
