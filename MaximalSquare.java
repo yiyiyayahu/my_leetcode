ea21bb1031307b447720c089c5fb5571f9dee6c2
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-03 15:36:06 -0700

    Update MaximalSquare.java

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -21,6 +21,36 @@ dp[i][j] = 0 if matrix[i][j]=='0'
 */
 public class Solution {
     public int maximalSquare(char[][] matrix) {
+      if(matrix == null || matrix.length == 0) return 0;
+      int rows = matrix.length, cols = matrix[0].length;
+      int[][] dp = new int[rows][cols];
+      int maxSize = 0;
+    
+      for(int j = 0; j < cols; j++) {
+        dp[0][j] = matrix[0][j] - '0';
+        maxSize = Math.max(maxSize, dp[0][j]);
+      }
+      for(int i = 0; i < rows; i++) {
+        dp[i][0] = matrix[i][0] - '0';
+        maxSize = Math.max(maxSize, dp[i][0]);
+      }
+      for(int i = 1; i < rows; i++) {
+        for(int j = 1; j < cols; j++) {
+          if(matrix[i][j] == '1') {
+            dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+            maxSize = Math.max(maxSize, dp[i][j]);
+          }
+        }
+      }
+      return maxSize*maxSize;
+    }
+}
+
+/*
+简单粗暴
+*/
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
         if(matrix == null) return 0;
         int result = 0;
         for(int i = 0 ; i < matrix.length; i++) {
