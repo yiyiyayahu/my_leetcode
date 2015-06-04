f8d6ce47effa7349a154d4fc6c02360af24d4257
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-03 19:20:48 -0700

    Update AddAndSearchWord-Data structure design.java

--- AddAndSearchWord-Data structure design.java
+++ AddAndSearchWord-Data structure design.java
@@ -24,6 +24,70 @@ You should be familiar with how a Trie works. If not, please work on this proble
 */
 
 /*
+好神奇，如果用eclipse就没什么问题
+后来我在boolean的found那里加了一个wrapper过了
+*/
+class TrieNode {
+    HashMap<Character, TrieNode> map = new HashMap<Character, TrieNode>();
+    boolean isLeaf;
+    public TrieNode() {
+
+    }
+}
+class Wrapper {
+	boolean found;
+	public Wrapper() {
+		
+	}
+}
+
+public class WordDictionary {
+    private TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        if(word == null) return;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(curr.map.containsKey(c)) {
+                curr = curr.map.get(c);
+            } else {
+                TrieNode n = new TrieNode();
+                curr.map.put(c, n);
+                curr = n;
+            }
+        }
+        curr.isLeaf = true;
+    }
+
+    public boolean search(String word) {
+    	Wrapper w = new Wrapper();
+        searchNode(word, root, 0, w);
+        return w.found;
+    }
+
+    public void searchNode(String word, TrieNode n, int index, Wrapper w) {
+        TrieNode curr = n;
+        if(index >= word.length()) {
+            if(curr.isLeaf) w.found = true;
+            return;
+        }
+        char c = word.charAt(index);
+        if(c != '.') {
+            if(curr.map.containsKey(c)) {
+                searchNode(word, curr.map.get(c), index+1, w);
+            }
+        } else {
+            for(Map.Entry<Character,TrieNode> entry : curr.map.entrySet()) {
+                searchNode(word, entry.getValue(), index+1, w);
+            }
+        }
+    }
+}
+/*
 下面的代码在一共跪了两个testcase，其中一个是：
 Input: 	addWord("ran"),addWord("rune"),addWord("runner"),addWord("runs"),addWord("add"),addWord("adds"),addWord("adder"),addWord("addee"),search("r.n"),search("ru.n.e"),search("add"),search("add."),search("adde."),search(".an."),search("...s"),search("....e."),search("......."),search("..n.r")
 Output: 	[false,false,true,true,true,false,true,true,false,false]
