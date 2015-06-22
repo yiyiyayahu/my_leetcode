8a2fff9494e6d4c7de740fb8e733d54909198ad2
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-21 18:50:31 -0700

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -8,8 +8,18 @@ When the cache reached its capacity, it should invalidate the least recently use
 
 /*
 这道题主要考察data structure
+如果是least recently used，那就是每次不管是set还是get，访问了就要放到前面来，这样当capacity达到的时候就删除最后一个就可以了
+但是如果用linkedlist的话删除太麻烦，所以用双链表（插入和删除为O(1)）
+而且如果get想要O(1)的话需要用到hashmap
 
-
+所以思路是：Hashmap+double linked list
+1. set：如果map里面有，更新map，把node挪到前面
+	如果没有，if比capacity小，加进去，更新len，else，把最后那个删掉，加进去
+2. get: 如果map有，把node挪到前面，返回node的值。
+	如果没有，返回-1
+	
+但是双链表的插入删除操作要注意，我写了好多次都错了，囧
+注意prev和next的更新！还有考虑head和end是不是null的问题
 */
 class Node {
     int key;
@@ -78,7 +88,7 @@ public class LRUCache {
     }
     
     public void addToHead(Node n) {
-      n.prev = null; //important!!! Since sometimes the node prev is not null, if move to front, set it to null
+        n.prev = null; //important!!! Since sometimes the node prev is not null, if move to front, set it to null
     	if(head != null) {
     		n.next = head;
     		head.prev = n;
